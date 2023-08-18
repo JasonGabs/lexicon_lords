ed52e192ab0b4e0f43d3f7814a323a8fb7c1c583
evan-pieper <evanjpieper@gmail.com>
2023-08-17 17:57:50 -0700

    added server.js file and updated dependencies

---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+// Import express-session
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions
+const sess = {
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
