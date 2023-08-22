ae2be439e8752927f00a68810b28b1f0f49851b1
evan-pieper <evanjpieper@gmail.com>
2023-08-21 19:58:53 -0700

    replaced project with proper CrossSet verbage

---
+++ controllers/api/CrossSetRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { CrossSet } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newCrossSet = await CrossSet.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newCrossSet);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const CrossSetData = await CrossSet.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!CrossSetData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(CrossSetData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
