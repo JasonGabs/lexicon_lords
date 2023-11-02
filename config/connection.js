a3dae64de53a94a5a7452377074ae272e9081c4d
evan-pieper <evanjpieper@gmail.com>
2023-11-01 23:14:33 -0700

    modified connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_MARIA_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
